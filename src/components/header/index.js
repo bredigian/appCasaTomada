e257b6a6a90dbcb4aff8420fe596e7e25c0fce26
Gianluca Bredice - Windows 10 Desktop <bredicegian@gmail.com>
2022-12-22 11:51:40 -0300

    fix align header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,16 +1,18 @@
-import { Image, Text, View } from "react-native"
+import { Image, SafeAreaView, Text, View } from "react-native"
 
 import React from "react"
 import { styles } from "./styles"
 
 const Header = () => {
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.headerLogo}
-        source={require("../../../assets/img/logo/logoCasatomada.png")}
-      />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Image
+          style={styles.headerLogo}
+          source={require("../../../assets/img/logo/logoCasatomada.png")}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -4,6 +4,8 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
   },
   headerLogo: {
     width: 70,
