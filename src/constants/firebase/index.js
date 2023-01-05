23d9e9964e3493d1a37e178af6b995fb058384a7
Gianluca Bredice <bredicegian@gmail.com>
2023-01-05 14:42:06 -0300

    protect api keys on constants of firebase and maps

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,5 +1,5 @@
-export const API_KEY = "AIzaSyDGycCuh6T41WoM2FMfxGcPI3qJJFW82WY"
+import Constants from "expo-constants"
 export const URL_BASE =
   "https://reactnative-casatomada-default-rtdb.firebaseio.com"
-export const URL_AUTH_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`
-export const URL_AUTH_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`
+export const URL_AUTH_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${Constants.manifest.extra.firebaseApiKey}`
+export const URL_AUTH_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${Constants.manifest.extra.firebaseApiKey}`
--- src/constants/maps/index.js
+++ src/constants/maps/index.js
@@ -1,8 +1,8 @@
-export const MAPS_API_KEY = "AIzaSyDzX2p4WFCclYBsvVYmEYrmnz7Acj25InY"
+import Constants from "expo-constants"
 export const MAPS_URL_BASE = `https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
 &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
 &markers=color:red%7Clabel:C%7C40.718217,-73.998284
-&key=${MAPS_API_KEY}`
+&key=${Constants.manifest.extra.mapsApiKey}`
 export const URL_GEOCODING = (lat, lng) => {
-  return `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${MAPS_API_KEY}`
+  return `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${Constants.manifest.extra.mapsApiKey}`
 }
